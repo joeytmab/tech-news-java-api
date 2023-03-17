602df745d35071eda1887d6425b26a3077c1b232
Joey Binas <joseph.t.binas@gmail.com>
2023-03-16 17:14:40 -0700

    added interfaces

---
+++ src/main/java/com.technews/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import com.technews.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findUserbyEmail(String email) throws Exception;
+
+
+}
